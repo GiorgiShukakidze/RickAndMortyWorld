6d1ee21ebd1d6b2854cf35d0440653efc8f0fa66
Giorgi Shukakidze <gshukakidze@gmail.com>
2021-08-18 19:39:02 +0200

    Initial Commit

---
+++ RickAndMortyWorld/RickAndMortyWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyWorldApp.swift
+//  RickAndMortyWorld
+//
+//  Created by Giorgi Shukakidze on 18.08.21.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
